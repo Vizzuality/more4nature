de3159b26637c7616d0253ce56d38b24aecbb1fb
David Inga <david.inga@vizzuality.com>
2024-01-15 13:40:28 +0100

    no js code needed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
 import css from "./main.css";
-
-function beforeUnloadListener(event) {
-  event.preventDefault();
-  return event.returnValue = 'Are you sure you want to exit?';
-};
-
-// A function that invokes a callback when the page has unsaved changes.
-onPageHasUnsavedChanges(() => {
-  window.addEventListener('beforeunload', beforeUnloadListener);
-});
-
-// A function that invokes a callback when the page's unsaved changes are resolved.
-onAllChangesSaved(() => {
-  window.removeEventListener('beforeunload', beforeUnloadListener);
-});
